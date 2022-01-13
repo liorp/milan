1e97e90d12eb8704640ff2e7ee732a1cd25fab7a
Lior Pollak <4294489+liorp@users.noreply.github.com>
2022-01-13 12:43:43 +0200

    ui: add space

--- components/GameExplanation.tsx
+++ components/GameExplanation.tsx
@@ -125,7 +125,7 @@ export const GameExplanation = () => {
                                     data-tip={
                                         'גרסה: ' +
                                         process.env.NEXT_PUBLIC_VERSION +
-                                        ' קומיט:' +
+                                        ' קומיט: ' +
                                         process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.slice(
                                             -7
                                         )
