73bb0ae7e82409a1da998ab9fb7bdca83b983a36
Lior Pollak <4294489+liorp@users.noreply.github.com>
2022-01-17 21:27:07 +0200

    fix: style of not in dictionary

--- components/WordNotInDictionary.tsx
+++ components/WordNotInDictionary.tsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 export const WordNotInDictionary = ({ word }: { word: string }) => {
     return (
-        <div
-            className={`alert alert-error absolute bg-opacity-95 font-bold text-center text-red-900 z-10 top-3 left-[24vw] w-[52vw]`}
-        >
-            <div className="flex flex-1 justify-between">
-                <label>
-                    מה זה {word}?<br />
-                    זה לא ברשימות שלי 😢
-                </label>
-            </div>
+        <div className="alert alert-error absolute bg-opacity-95 font-bold text-center text-red-900 z-10 top-3 word-not-in-dictionary justify-center">
+            <label>
+                מה זה {word}?<br />
+                זה לא ברשימות שלי 😢
+            </label>
         </div>
     )
 }
